7092ab1faf112bbb159a7d9724cc836dfc28fde9
Hye Soo Park <flyjwayur@gmail.com>
2019-02-12 02:42:32 +0200

    Add note

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
 import Notes from './components/Notes/index';
+import notes from './data/notes';
+
+import uuid from 'uuid';
 
 import './App.css';
 
 class App extends Component {
+  state = {
+    notes,
+  };
+
+  addNote = () => {
+    this.setState({
+      notes: [
+        ...this.state.notes,
+        {
+          id: uuid.v4(),
+          task: 'New task wow',
+        },
+      ],
+    });
+  };
+
   render() {
-    return <Notes />;
+    const { notes } = this.state;
+
+    return (
+      <div>
+        <button onClick={this.addNote}>+</button>
+        <Notes notes={notes} />
+      </div>
+    );
   }
 }
 
