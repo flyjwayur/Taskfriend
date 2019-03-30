32747023e280e6e0e7cdeef6b2e5506c969c473a
Hye Soo Park <flyjwayur@gmail.com>
2019-03-30 11:16:13 +0200

    Make notes react to the other notes being dragged/hovered/dropped

--- src/components/Kanban/components/Note/index.js
+++ src/components/Kanban/components/Note/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { DragSource } from 'react-dnd';
+import { compose } from 'redux';
+import { DragSource, DropTarget } from 'react-dnd';
 import ItemTypes from '../../constants/itemTypes';
 import './styles.scss';
 
-const Note = ({ connectDragSource, children, ...props }) => {
-  return connectDragSource(
+const Note = ({ connectDragSource, connectDropTarget, children, ...props }) => {
+  return compose(
+    connectDragSource,
+    connectDropTarget
+  )(
     <div className="note" {...props}>
       {children}
     </div>
@@ -18,6 +22,18 @@ const noteSource = {
   }
 };
 
-export default DragSource(ItemTypes.NOTE, noteSource, connect => ({
-  connectDragSource: connect.dragSource()
-}))(Note);
+const noteTarget = {
+  hover(targetProps, monitor) {
+    const sourceProps = monitor.getItem();
+    console.log('dragging note', sourceProps, targetProps);
+  }
+};
+
+export default compose(
+  DragSource(ItemTypes.NOTE, noteSource, connect => ({
+    connectDragSource: connect.dragSource()
+  })),
+  DropTarget(ItemTypes.NOTE, noteTarget, connect => ({
+    connectDropTarget: connect.dropTarget()
+  }))
+)(Note);
